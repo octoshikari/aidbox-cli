4ae481ee53d4e2e84a9c0728ff098440a4854ae5
Alexander Streltsov <funyloony@gmail.com>
2022-07-22 23:00:39 +0300

    refactor: remove ui

--- ui/src/model.ts
+++
@@ -1,9 +0,0 @@
-import { createEvent, createStore } from "effector";
-import { persist } from "effector-storage/local";
-
-export const navbarToggle = createEvent();
-
-export const $isNavbarOpened = createStore<boolean>(false)
-  .on(navbarToggle, (current) => !current);
-
-persist({ store: $isNavbarOpened });
--- ui/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { babel } from "@rollup/plugin-babel";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [babel({ extensions: [".ts"], babelHelpers: "bundled" }), react()],
-});
